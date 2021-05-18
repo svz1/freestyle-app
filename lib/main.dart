9105b5d9129656c0864b28fb864d91074cf12aac
svz1 <saviosabu1@gmail.com>
2021-05-18 23:18:01 +0530

    trial

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( appBar:appBar(title)
 
     ) ;
   }
