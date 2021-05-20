ba2ff799d6e8bd4186feb9baf0748cb94a6bdd13
svz1 <saviosabu1@gmail.com>
2021-05-20 13:24:23 +0530

    initialising firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' ;
+import 'package:firebase_core/firebase_core.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(LandingPage());
+}
 
-void main()=>runApp(MaterialApp(
-  home:Home()
-)) ;
-class Home extends StatelessWidget {
+
+
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization =Firebase.initializeApp() ; 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(child: Text("hello heyy"))
+    return FutureBuilder(
+      future:_initialization,
+      builder:(context,snapshot) {
+        if(snapshot.hasError) {
+          return Scaffold(body: Center(child:Text("error: ${snapshot.error}"),
+          ),
+          ); 
+
+        }
+        if(snapshot.connectionState==ConnectionState.done) {
+          return Scaffold(
+            body:Container(
+              child: Center(
+                child: Text("Firebase App Initialised"),
+          ),
+          ),
+          );
+        }       
+
+
+        
+          return Scaffold(body:
+          Container(child: Center(child: Text("initialising app"),
+          ),
+          ),
+          );
+      }
+      
       
     );
   }
