be03f3a8c5417cafb208f4d3e1e7a0e611984eae
svz1 <saviosabu1@gmail.com>
2021-05-20 13:33:15 +0530

    firebase app succesfully initialised

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(LandingPage());
+  runApp(MyApp());
 }
-
+ class MyApp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(home: LandingPage(),
+       
+     );
+   }
+ }
 
 
 
